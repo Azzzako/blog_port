ab58e57e00592282ad685d1078d656f1c666b6fe
Asael Hernandez <zanduva@gmail.com>
2023-09-22 16:35:11 -0600

    tailwind, eslint, mdx config add

---
+++ .eslintrc.js
@@ -0,0 +1,32 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: [
+    'airbnb',
+    'airbnb/hooks',
+    'plugin:react/recommended',
+    'plugin:jsx-a11y/recommended',
+  ],
+  overrides: [
+    {
+      env: {
+        node: true,
+      },
+      files: [
+        '.eslintrc.{js,cjs}',
+      ],
+      parserOptions: {
+        sourceType: 'script',
+      },
+    },
+  ],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  rules: {
+    // Aquí puedes agregar reglas personalizadas o anular reglas existentes.
+  },
+};
