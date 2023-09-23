3125ee82f5d6e912808ecfe34032f76fc2336de7
Asael Hernandez <zanduva@gmail.com>
2023-09-22 19:18:34 -0600

    navbar create

--- src/components/header.js
+++ src/components/header.js
@@ -1,17 +1,27 @@
 import * as React from "react"
 import { Link } from "gatsby"
-import { animated, useSpring } from "@react-spring/web"
-
+import { LiaDev } from "react-icons/lia"
+import { Parallax, ParallaxLayer } from '@react-spring/parallax';
 
 const Header = () => {
-  
   return (
-    <header className="bg-red-400">
-      <div>
-        
-      </div>
-     
-    </header>
+    <Parallax pages={1}>
+      <ParallaxLayer offset={0} speed={0.5}>
+        <header className="bg-red-300 flex flex-row justify-around h-[90px] w-full">
+          <div className="flex items-center">
+            <LiaDev size={80} />
+          </div>
+
+          <div>
+            <ul className="flex flex-row w-full items-center h-full gap-10">
+              <li>About</li>
+              <li>Projects</li>
+              <li>Contact</li>
+            </ul>
+          </div>
+        </header>
+      </ParallaxLayer>
+    </Parallax>
   )
 }
 
