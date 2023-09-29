98b31fc63f454fa185654399a84a9e5154e56819
Asael Hernandez <zanduva@gmail.com>
2023-09-29 13:51:34 -0600

    estoy hasta la madre

--- gatsby-config.js
+++ gatsby-config.js
@@ -16,20 +16,20 @@ module.exports = {
   },
   plugins: [
     'gatsby-plugin-mdx',
-    {
-      resolve: 'gatsby-source-filesystem',
-      options: {
-        name: 'posts',
-        path: `${__dirname}/src/posts`,
-      },
-    },
+    // {
+    //   resolve: 'gatsby-source-filesystem',
+    //   options: {
+    //     name: 'posts',
+    //     path: `${__dirname}/src/posts`,
+    //   },
+    // },
     
-    {
-      resolve: 'gatsby-plugin-page-creator',
-      options: {
-        path: `${__dirname}/src/posts`,
-      },
-    },
+    // {
+    //   resolve: 'gatsby-plugin-page-creator',
+    //   options: {
+    //     path: `${__dirname}/src/posts`,
+    //   },
+    // },
     'gatsby-plugin-image',
     {
       resolve: 'gatsby-source-filesystem',
