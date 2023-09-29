75cc8f3e79d8ffb18f2cc3be9e5fb31f4a276a9c
Asael Hernandez <zanduva@gmail.com>
2023-09-29 14:04:33 -0600

    nodebi borrar ni verga

---
+++ src/templates/using-dsg.js
@@ -0,0 +1,14 @@
+import React from "react"
+import Layout from "../components/layout"
+
+const UsingDefPage = () => (
+  <Layout>
+    <h1>Using Gatsby Default Starter</h1>
+    <p>
+      Welcome to your new Gatsby site. This is the "using-def.js" page. You can
+      edit this page content in the file "src/pages/using-def.js".
+    </p>
+  </Layout>
+)
+
+export default UsingDefPage
